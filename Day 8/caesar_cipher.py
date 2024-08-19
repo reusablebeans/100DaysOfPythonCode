0b9e17cab18a6b4e32dedbf8d3e08ec1c50fd67f
Areeb <152879951+reusablebeans@users.noreply.github.com>
2024-08-19 13:09:02 -0500

    Day 8 - Caesar Cipher
    Creating Cipher from functions with inputs. Also utilizing the strings' index function.

---
+++ Day 8/caesar_cipher.py
@@ -0,0 +1,39 @@
+import caesar_cipher_art
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+    for letter in original_text:
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+again = True
+
+while again:
+    print(caesar_cipher_art.logo)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    restart = input("Would you like to encode or decode another message? (Y/N)\n")
+    if restart.lower() == "y" or restart.lower() == "yes":
+        again = True
+    else:
+        again = False
+        print("Thank you for using the Caesar Cipher!")
+
+
+
+
+
